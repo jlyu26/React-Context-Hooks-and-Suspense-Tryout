641105dcef99dcdd093c1bb6d306894dc9bfbe2f
Jinyan Lyu <lyujinyan@gmail.com>
2019-12-19 20:38:41 -0500

    Consume context using `Context.Consumer`.

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
-  static contextType = ThemeContext;
+  // static contextType = ThemeContext;
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
-        <ul>
-          <li style={{ background: theme.ui }}>book1</li>
-          <li style={{ background: theme.ui }}>book2</li>
-          <li style={{ background: theme.ui }}>book3</li>
-        </ul>
-      </div>
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+              <ul>
+                <li style={{ background: theme.ui }}>book1</li>
+                <li style={{ background: theme.ui }}>book2</li>
+                <li style={{ background: theme.ui }}>book3</li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
-  static contextType = ThemeContext;
+  // static contextType = ThemeContext;
   render() {
     // console.log(this.context);
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
+
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      // 1. can be used in function component as well
+      // 2. can consume multiple contexts in one component
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
