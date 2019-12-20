6980255d3442a4907c5d6b1ec4d86b46240bdf0f
Jinyan Lyu <lyujinyan@gmail.com>
2019-12-20 13:15:58 -0500

    Initialize hooks app.

---
+++ hooksapp/src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = () => {
+  return (
+    <div className="song-list">
+      <ul>
+        <li>song 1</li>
+        <li>song 2</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SongList;
