b0ab51a966911e170a06fdfc6607b99472dce51c
Jinyan Lyu <lyujinyan@gmail.com>
2019-12-21 00:52:37 -0500

    `useState` hook inside functional component.

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid/v1';
 
 const SongList = () => {
+  // hooks allows function components to use states
+  // `useState` returns array of two value
+  // first is the state data itself, in this case is the array of objects
+  // second is the function to edit the state
+  // use array destracturing to assign them
+  const [songs, setSongs] = useState([
+    { title: 'song 1', id: 1 },
+    { title: 'song 2', id: 2 },
+    { title: 'song 3', id: 3 }
+  ]);
+  const addSong = () => {
+    // pass in the new state data which is going to completely replace the old one
+    setSongs([...songs, { title: 'new song', id: uuid() }]);
+  };
   return (
     <div className="song-list">
       <ul>
-        <li>song 1</li>
-        <li>song 2</li>
+        {songs.map(song => {
+          return <li key={song.id}>{song.title}</li>;
+        })}
       </ul>
+      <button onClick={addSong}>Add song</button>
     </div>
   );
 };
