c0351e83d237e0af6701fb4fc842418b80539740
Jinyan Lyu <lyujinyan@gmail.com>
2020-01-13 13:09:16 -0500

    clean-start

--- react-hooks-note/src/App.js
+++ react-hooks-note/src/App.js
@@ -1,25 +1,7 @@
 import React, { useState } from 'react';
-import InfoTag from './components/infoTag';
-import useProfile from './hooks/useProfile';
-
-const initialProfile = {
-  name: 'john',
-  age: 30,
-  address: 'New York, NY',
-  married: true
-};
 
 function App() {
-  const { profile, removeItem } = useProfile(initialProfile);
-  const removeItemHandle = e => {
-    removeItem(e.target.name);
-  };
-
-  return Object.entries(profile).map((item, index) => {
-    return (
-      <InfoTag key={`${index}.${item[0]}.${item[1]}`} {...{ item }} onClick={removeItemHandle} />
-    );
-  });
+  return <div className="App"></div>;
 }
 
 export default App;
