406eb8989aa525fc8cda8710d7397cc697f0db43
Jinyan Lyu <lyujinyan@gmail.com>
2020-01-13 18:27:37 -0500

    Link and NavLink.

--- react-hooks-note/src/App.js
+++ react-hooks-note/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link, NavLink } from 'react-router-dom';
 import About from './pages/About';
 
+// Can also use Link, but doesn't have activeClass or activeStype from NavLink
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <ul>
+          <li>
+            <NavLink to="/" exact activeStyle={{ color: 'green' }}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" exact activeStyle={{ color: 'green' }}>
+              About
+            </NavLink>
+          </li>
+        </ul>
         <Route
           exact
           path="/"
