b932db690b0c7462445a4c90d873af24c7361223
Jinyan Lyu <lyujinyan@gmail.com>
2020-01-13 16:15:28 -0500

    <BrowserRouter>, <Route path="..." render={Page}> or render = {() => { return JSX }}

--- react-hooks-note/src/App.js
+++ react-hooks-note/src/App.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import About from './pages/About';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return <div>welcome home</div>;
+          }}
+        ></Route>
+        <Route path="/about" render={About}></Route>
+        <Route
+          path="/user/:username"
+          render={({ match }) => {
+            return <div>welcome {match.params.username}</div>;
+          }}
+        ></Route>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
